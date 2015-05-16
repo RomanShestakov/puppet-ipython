c82871dfcdb2c3b11e3756fe22bf6c4d109cd700
RomanShestakov <romanshestakov@yahoo.co.uk>
2015-05-16 19:29:46 +0100

    fix typo

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
 class ipython::params {
-  $install_method = 'pip',
-  $ipython_version = '1.1.0',
-  $git_branch = 'master',
-  $git_tag = '',
-  $python_version = '2.7',
+  $install_method = 'pip'
+  $ipython_version = '1.1.0'
+  $git_branch = 'master'
+  $git_tag = ''
+  $python_version = '2.7'
   $manage_pip = false
 }
